ad775097b62240266002dc8c2915047e905c93c0
abhishek4922 <abhishekjain18138@gmail.com>
2025-10-06 19:05:23 +0530

    feat: Ignore app/build directory
    Former-commit-id: 04142bf3c3b1447e71c567d736db08ccbe462866
    Former-commit-id: c7559df092cfdad325ae2028f58866de38027f21

--- app/build/intermediates/cxx/Debug/2g6k2k1e/logs/arm64-v8a/build_command_native-lib.bat
+++
@@ -1,5 +0,0 @@
-@echo off
-"C:\\Users\\abhis\\AppData\\Local\\Android\\Sdk\\cmake\\3.18.1\\bin\\ninja.exe" ^
-  -C ^
-  "C:\\Users\\abhis\\Desktop\\Projects\\web\\proj\\flamwind\\app\\.cxx\\Debug\\2g6k2k1e\\arm64-v8a" ^
-  native-lib
